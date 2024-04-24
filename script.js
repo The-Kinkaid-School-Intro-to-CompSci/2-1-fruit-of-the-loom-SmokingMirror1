8cc30ba47aa531d1e9f750b472372558c808e8fe
Ella Gillentine <ella.gillentine@Ellas-MBP-3.attlocal.net>
2024-04-23 20:32:50 -0500

    a late turn in

--- script.js
+++ script.js
@@ -14,8 +14,35 @@ function clearCards() {
     
 }
 
+function addFruitCard(fruit){
+    console.log(fruit.name)
+    let newFruitCard = document.createElement('div');
+
+    let fruitHeading = document.createElement('h3');
+    fruitHeading.textContent = fruit.name;
+    newFruitCard.appendChild(fruitHeading)
+
+    let fruitColor = document.createElement('p')
+    fruitColor.textContent = `Color: ${fruit.color}`
+    fruitColor.style.color = fruit.color
+    newFruitCard.appendChild(fruitColor)
+
+    let fruitShape = document.createElement('p')
+    fruitShape.textContent = `Shape: ${fruit.shape}`
+    newFruitCard.appendChild(fruitShape)
+
+    let fruitEmoji = document.createElement('p')
+    fruitEmoji.textContent = `Emoji: ${fruit.emoji}`
+    newFruitCard.appendChild(fruitEmoji)
+
+    let fruitContainer = document.querySelector('#cards')
+    newFruitCard.classList.add('fruitCard')
+    fruitContainer.appendChild(newFruitCard)
+}
+
 function runProgram() {
     console.log("Program is running");
+    addFruitCard(fruitObjects[0])
 
 }
 
